c1035486ef967e2c017bb128933d5d07e14e34e3
chrismapakha <chrismapakha@uchicago.edu>
2017-06-20 17:03:42 -0500

    Indentation

--- loop.c
+++ loop.c
@@ -8,79 +8,79 @@
 
 void* three_macs(void * pn_iter)
 {
-	int * pn_iter_2 = (int * ) pn_iter;
-	int n_iter = * pn_iter_2;
+  int * pn_iter_2 = (int * ) pn_iter;
+  int n_iter = * pn_iter_2;
 
-	int iter = 0;
-	float a = 1.2;
-	float b = 2.3;
-	float c = a;
+  int iter = 0;
+  float a = 1.2;
+  float b = 2.3;
+  float c = a;
 
-	for(iter = 0; iter < n_iter; iter++)
-	{
-		c = c * b + iter ;
-		c = c * b + iter ;
-		c = c * b + iter ;
-	}  
+  for(iter = 0; iter < n_iter; iter++)
+  {
+    c = c * b + iter ;
+    c = c * b + iter ;
+    c = c * b + iter ;
+  }  
 }
 
 int main(int argc, char ** argv)
 {
 
-	if(argc != 3 && argc != 1)
-	{
-		printf("Usage : ./loop [number of iteration : default = 500000000] [number of thread : default = 1]\n");
-		exit(EXIT_FAILURE);
-	}
-	int n_iter = 0;
-	int n_thread = 0;
-	if(argc == 3)
-	{
-		n_iter = atoi(argv[1]);	
-		n_thread = atoi(argv[2]);
-	}
-	else
-	{
-		n_iter = 500000000; // Around 9.5-10 second
-		n_thread = 1;
-	}
-	
-	/* Time here */
-	struct timespec startTime;
-	struct timespec endTime;
-	memset(&startTime, 0, sizeof(startTime));
-	memset(&endTime, 0, sizeof(endTime));
+  if(argc != 3 && argc != 1)
+  {
+    printf("Usage : ./loop [number of iteration : default = 500000000] [number of thread : default = 1]\n");
+    exit(EXIT_FAILURE);
+  }
+  int n_iter = 0;
+  int n_thread = 0;
+  if(argc == 3)
+  {
+    n_iter = atoi(argv[1]); 
+    n_thread = atoi(argv[2]);
+  }
+  else
+  {
+    n_iter = 500000000; // Around 9.5-10 second
+    n_thread = 1;
+  }
+  
+  /* Time here */
+  struct timespec startTime;
+  struct timespec endTime;
+  memset(&startTime, 0, sizeof(startTime));
+  memset(&endTime, 0, sizeof(endTime));
 
-	long int lStart_t = 0;
-	long int lEnd_t = 0;
+  long int lStart_t = 0;
+  long int lEnd_t = 0;
 
-	
-	int iter_thread = 0;
-    pthread_t* tid = malloc(sizeof(pthread_t) * n_thread);
+  
+  int iter_thread = 0;
+  pthread_t* tid = malloc(sizeof(pthread_t) * n_thread);
     
-    clock_gettime(CLOCK_MONOTONIC, &startTime);
-    for(iter_thread = 0; iter_thread<n_thread; iter_thread++)
-    {
-        if(pthread_create(&tid[iter_thread], NULL, three_macs, (void*)&n_iter))
-        {
-            printf("%i thread failure\n",iter_thread);
-        }
-    }
+  clock_gettime(CLOCK_MONOTONIC, &startTime);
+  for(iter_thread = 0; iter_thread<n_thread; iter_thread++)
+  {
+      if(pthread_create(&tid[iter_thread], NULL, three_macs, (void*)&n_iter))
+      {
+        printf("%i thread failure\n",iter_thread);
+      }
+  }
     
-    for(iter_thread = 0; iter_thread<n_thread; iter_thread++)
-    {
-        pthread_join(tid[iter_thread], NULL);
-    }
-    clock_gettime(CLOCK_MONOTONIC, &endTime);
+  for(iter_thread = 0; iter_thread<n_thread; iter_thread++)
+  {
+    pthread_join(tid[iter_thread], NULL);
+  }
+  clock_gettime(CLOCK_MONOTONIC, &endTime);
 
-    free(tid);	
+  free(tid);  
 
-	/* Measure the time */
-	lStart_t = startTime.tv_sec * SECOND_2_NANOS + startTime.tv_nsec;	
-	lEnd_t = endTime.tv_sec * SECOND_2_NANOS + endTime.tv_nsec;	
-	
-	printf("%d,%d,%lf\n", n_iter, n_thread, (float)(lEnd_t - lStart_t)/SECOND_2_NANOS);
+  /* Measure the time */
+  lStart_t = startTime.tv_sec * SECOND_2_NANOS + startTime.tv_nsec; 
+  lEnd_t = endTime.tv_sec * SECOND_2_NANOS + endTime.tv_nsec; 
+  
+  printf("%d,%d,%lf\n", n_iter, n_thread, (float)(lEnd_t - lStart_t)/SECOND_2_NANOS);
 
-	exit(EXIT_SUCCESS);
+  exit(EXIT_SUCCESS);
 }
 
--- loop_process.c
+++ loop_process.c
@@ -4,20 +4,21 @@
 
 int main (int argc, char ** argv)
 {
-    if(argc != 2)
-    {
-        printf("Usage : ./loop_process number_of_process \n");
-        exit(EXIT_FAILURE);
-    }
-    int iter = 0;
-    int n_process = atoi(argv[1]);
-    char command[100];
+  if(argc != 2)
+  {
+    printf("Usage : ./loop_process number_of_process \n");
+    exit(EXIT_FAILURE);
+  }
+  
+  int iter = 0;
+  int n_process = atoi(argv[1]);
+  char command[100];
 
-    for (iter = 0; iter <n_process; iter++)
-    {
-        sprintf(command,"./loop &");
-        system(command);
-    }
+  for (iter = 0; iter <n_process; iter++)
+  {
+    sprintf(command,"./loop &");
+    system(command);
+  }
 
-    exit(EXIT_SUCCESS);
+  exit(EXIT_SUCCESS);
 }
